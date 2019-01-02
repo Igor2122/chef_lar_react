27a1904edb16018235eb815672a5b679e617b30f
igor2122 <igor_pavlenko@yahoo.com>
2019-01-02 14:01:14 +0100

    deletion

--- public/js/app.js
+++ public/js/app.js
@@ -61457,8 +61457,8 @@ function (_React$Component) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _About_module_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./About.module.scss */ "./resources/js/containers/About/About.module.scss");
-/* harmony import */ var _About_module_scss__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_About_module_scss__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _About_module_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./About.module.scss */ "./resources/js/containers/About/About.module.scss");
+/* harmony import */ var _About_module_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_About_module_scss__WEBPACK_IMPORTED_MODULE_1__);
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -61495,7 +61495,7 @@ function (_Component) {
     key: "render",
     value: function render() {
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
-        className: _About_module_scss__WEBPACK_IMPORTED_MODULE_2___default.a.TextColor
+        className: _About_module_scss__WEBPACK_IMPORTED_MODULE_1___default.a.TextColor
       }, "About us content will come here"));
     }
   }]);
