8d694bc9924c754011742b3d1cfa2659561e1ea5
Igor <igor_pavlenko@yahoo.com>
2019-03-10 13:54:47 +0100

    added ingredients category & ingredients migraiton

---
+++ api_marw_lv/database/migrations/2019_03_10_124357_create_ingredients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('recepies_id');
+            $table->char('name', 100);
+            $table->integer('ingridientCategory_id');
+            $table->integer('calories');
+            $table->integer('protein');
+            $table->integer('carbs');
+            $table->integer('fat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
